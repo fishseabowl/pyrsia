cf58204db73775f2c8e7a328a2151780bedb9124
fishseabowl <xuefeih@jfrog.com>
2023-01-03 15:42:01 -0800

    Add spawner

--- src/blockchain/src/aleph_bft/network.rs
+++ src/blockchain/src/aleph_bft/network.rs
@@ -23,8 +23,14 @@ use crate::{
 };
 
 use super::dataio;
-use aleph_bft::{NodeIndex, TaskHandle};
-use futures::channel::mpsc::{self, UnboundedReceiver, UnboundedSender};
+use aleph_bft::{NodeIndex, SpawnHandle, TaskHandle};
+use futures::{
+    channel::{
+        mpsc::{self, UnboundedReceiver, UnboundedSender},
+        oneshot,
+    },
+    Future,
+};
 use log::{trace, warn};
 use parity_scale_codec::{Decode, Encode};
 
@@ -78,4 +84,32 @@ pub struct NetworkManager {
     consensus_rx: UnboundedReceiver<(NetworkData, Recipient)>,
     block_tx: UnboundedSender<Block>,
     block_rx: UnboundedReceiver<Block>,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Default, Decode, Encode)]
+pub struct Spawner;
+
+impl SpawnHandle for Spawner {
+    fn spawn(&self, _name: &str, task: impl Future<Output = ()> + Send + 'static) {
+        tokio::spawn(task);
+    }
+
+    fn spawn_essential(
+        &self,
+        _: &str,
+        task: impl Future<Output = ()> + Send + 'static,
+    ) -> TaskHandle {
+        let (res_tx, res_rx) = oneshot::channel();
+        tokio::spawn(async move {
+            task.await;
+            res_tx.send(()).expect("We own the rx.");
+        });
+        Box::pin(async move { res_rx.await.map_err(|_| ()) })
+    }
+}
+
+impl Spawner {
+    pub fn new() -> Self {
+        Spawner {}
+    }
 }
